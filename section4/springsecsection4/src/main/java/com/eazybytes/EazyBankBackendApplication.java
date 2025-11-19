a9a69d4262a8ad11e243c859564c9347b4fd7c5f
dahee <meluna0226@gmail.com>
2025-11-19 21:12:17 +0900

    add: Section4 실습 코드 업로드

--- section4/springsecsection4/src/main/java/com/eazybytes/EazyBankBackendApplication.java
+++ section4/springsecsection4/src/main/java/com/eazybytes/EazyBankBackendApplication.java
@@ -1,11 +1,12 @@
-package com.easybytes;
+package com.eazybytes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@ComponentScan - 메인 패키지 바깥에 있을 때 정의한다. 메인패키지는 SpringBootApplication이 있는 곳
+//@EnableWebSecurity // Spring Boot를 사용하는 경우에는 명시하지 않아도 된다. build.gradle에서 주입한거만으로도 적용이 됨.
 public class EazyBankBackendApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(EazyBankBackendApplication.class, args);
     }
--- section4/springsecsection4/src/main/resources/sql/scripts.sql
+++ section4/springsecsection4/src/main/resources/sql/scripts.sql
@@ -17,3 +17,15 @@ INSERT IGNORE INTO authorities VALUES('user', 'read');
 
 INSERT IGNORE INTO users VALUES ('admin', '{bcrypt}$2a$12$QghCYcYMQkauzbQi4XYj8O5TAFAfqZei16OivC8sNDTD.SRZU7/I6', '1');
 INSERT IGNORE INTO authorities VALUES('admin', 'admin');
+
+create table customer (
+    id int NOT NULL AUTO_INCREMENT,
+    email varchar(45) NOT NULL,
+    pwd varchar(200) NOT NULL,
+    role varchar(45) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO customer (email, pwd, role)VALUES ('happy@example.com', '{noop}EazyBytes@12345', 'read');
+INSERT INTO customer (email, pwd, role) VALUES ('admin@example.com', '{bcrypt}$2a$12$8PMkIZUPcaYSqdEMhCzL2uF2vpslZ2/0dIbYx2AjyfDtFzHsrT3hC', 'admin');
+
